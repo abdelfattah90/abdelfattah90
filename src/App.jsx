ec3e699b994b25dd1dee89561512af4aaf5e36d7
Ahmed Abdelfattah <ahmedabdelfattah0022@gmail.com>
2023-10-31 05:32:06 +0200

    projects

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <div className='container'>
           <div className='info'>
             <h1 className='name'>Ahmed Abdelfattah</h1>
-            <h4 className='job'>Backend developer</h4>
+            <h4 className='job'>React developer</h4>
           </div>
           <div className='image'>
             <img
@@ -28,20 +28,20 @@ function App() {
             <div className='p-5 col-sm-6'>
               <div className='card card-transition'>
                 <img
-                  src='./images/app.png'
+                  src='./images/projects/dashboard-ui-kit.png'
                   className='card-img-top'
                   alt='app'
                 />
                 <div className='card-body d-flex flex-column'>
-                  <h4 className='card-title'>eCommerce MERN Stack App</h4>
+                  <h4 className='card-title'>Next.js Dashboard UI Kit</h4>
                   <p className='card-text text-muted mb-4 p-3'>
-                    A full-stack ecommerce app that uses React, Redux, MongoDB,
-                    NodeJS, Typescript, ExpressJS, and JWT Authentication to
-                    upload and manage ecommerce data.
+                    Designed initial layout, integrated Components, suitable and
+                    responsive for all screens. Using Next.js, TypeScript,
+                    TailwindCSS, Syncfusion, Chart.js, Swiper.js, React icons.
                   </p>
                   <div className='btn-container'>
                     <a
-                      href='https://www.google.com'
+                      href='https://dashboard-ui-kit.vercel.app'
                       className='btn button-live'
                       target='_blank'
                       rel='noreferrer'
@@ -49,7 +49,7 @@ function App() {
                       Live
                     </a>
                     <a
-                      href='https://www.google.com'
+                      href='https://github.com/abdelfattah90'
                       className=''
                       target='_blank'
                       rel='noreferrer'
@@ -64,26 +64,108 @@ function App() {
                 </div>
               </div>
             </div>
-
             {/* End Card col- */}
+
             {/* Start Card col- */}
             <div className='p-5 col-sm-6'>
               <div className='card card-transition'>
                 <img
-                  src='./images/app.png'
+                  src='./images/projects/proshop.png'
                   className='card-img-top'
                   alt='app'
                 />
                 <div className='card-body d-flex flex-column'>
                   <h4 className='card-title'>eCommerce MERN Stack App</h4>
                   <p className='card-text text-muted mb-4 p-3'>
-                    A full-stack ecommerce app that uses React, Redux, MongoDB,
-                    NodeJS, Typescript, ExpressJS, and JWT Authentication to
-                    upload and manage ecommerce data.
+                    Shopping cart, product search, upload image, admin area and
+                    PayPal integration. Using React vite, Redux Toolkit, Axios,
+                    Node.js, Express, JWT, MongoDB.
+                  </p>
+                  <div className='btn-container'>
+                    <a
+                      href='https://proshop-tpqo.onrender.com'
+                      className='btn button-live'
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      Live
+                    </a>
+                    <a
+                      href='https://github.com/abdelfattah90'
+                      className=''
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      <img
+                        src='./images/github-dark.png'
+                        width={50}
+                        height={50}
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* End Card col- */}
+
+            {/* Start Card col- */}
+            <div className='p-5 col-sm-6'>
+              <div className='card card-transition'>
+                <img
+                  src='./images/projects/apple.png'
+                  className='card-img-top'
+                  alt='app'
+                />
+                <div className='card-body d-flex flex-column'>
+                  <h4 className='card-title'>Apple Store</h4>
+                  <p className='card-text text-muted mb-4 p-3'>
+                    Multi Languages app, manage and share state by Context API.
+                    Using Next.js, TypeScript, TailwindCSS, React-i18next.
+                  </p>
+                  <div className='btn-container'>
+                    <a
+                      href='https://appple.vercel.app'
+                      className='btn button-live'
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      Live
+                    </a>
+                    <a
+                      href='https://github.com/abdelfattah90'
+                      className=''
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      <img
+                        src='./images/github-dark.png'
+                        width={50}
+                        height={50}
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* End Card col- */}
+
+            {/* Start Card col- */}
+            <div className='p-5 col-sm-6'>
+              <div className='card card-transition'>
+                <img
+                  src='./images/projects/seira.png'
+                  className='card-img-top'
+                  alt='app'
+                />
+                <div className='card-body d-flex flex-column'>
+                  <h4 className='card-title lang-ar'>السيرة النبوية</h4>
+                  <p className='card-text text-muted mb-4 p-3'>
+                    Using flutter cross platform, shared preferences, and
+                    considerate interface segregation principle.
                   </p>
 
                   <a
-                    href='https://www.google.com'
+                    href='https://play.google.com/store/apps/details?id=com.seira.seiranabawayaa'
                     className='btn button-live'
                     target='_blank'
                     rel='noreferrer'
@@ -93,50 +175,6 @@ function App() {
                 </div>
               </div>
             </div>
-
-            {/* End Card col- */}
-
-            {/* Start Card col- */}
-            <div className='p-5 col-sm-6'>
-              <div className='card card-transition'>
-                <img
-                  src='./images/app.png'
-                  className='card-img-top'
-                  alt='app'
-                />
-                <div className='card-body d-flex flex-column'>
-                  <h4 className='card-title'>eCommerce MERN Stack App</h4>
-                  <p className='card-text text-muted mb-4 p-3'>
-                    A full-stack ecommerce app that uses React, Redux, MongoDB,
-                    NodeJS, Typescript, ExpressJS, and JWT Authentication to
-                    upload and manage ecommerce data.
-                  </p>
-                  <div className='btn-container'>
-                    <a
-                      href='https://www.google.com'
-                      className='btn button-live'
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      Live
-                    </a>
-                    <a
-                      href='https://www.google.com'
-                      className=''
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      <img
-                        src='./images/github-dark.png'
-                        width={50}
-                        height={50}
-                      />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
             {/* End Card col- */}
           </div>
         </section>
@@ -146,17 +184,17 @@ function App() {
         <h2 className='text-center p-4 mt-3'>Technologies and Tools</h2>
         <div className='tech'>
           <img src='./images/javascript.svg' height={70} width={70} />
-          <img src='./images/node.svg' height={70} width={70} />
-          <img src='./images/express.svg' height={70} width={70} />
-          <img src='./images/prisma.svg' height={70} width={70} />
-          <img src='./images/mysql.svg' height={70} width={70} />
-          <img src='./images/postgresql.svg' height={70} width={70} />
-          <img src='./images/mongo.svg' height={70} width={70} />
+          <img src='./images/typescript.svg' height={70} width={70} />
           <img src='./images/react.svg' height={70} width={70} />
           <img src='./images/next-js.svg' height={70} width={70} />
+          <img src='./images/tailwind-css.svg' height={70} width={70} />
           <img src='./images/redux.svg' height={70} width={70} />
-          <img src='./images/react-query.svg' height={70} width={70} />
-          <img src='./images/bootstrap.svg' height={70} width={70} />
+          <img src='./images/axios.svg' height={70} width={70} />
+          <img src='./images/postman.svg' height={70} width={70} />
+          <img src='./images/node.svg' height={70} width={70} />
+          <img src='./images/express.svg' height={70} width={70} />
+          <img src='./images/mysql.svg' height={70} width={70} />
+          <img src='./images/mongo.svg' height={70} width={70} />
         </div>
       </div>
 
