3ec8d18a8c9de0835db89816d2998c04e3f70d9b
Ahmed Abdelfattah <ahmedabdelfattah0022@gmail.com>
2023-10-31 06:15:25 +0200

    projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
+const projects = [
+  {
+    title: 'Next.js Dashboard UI Kit',
+    description:
+      'Designed initial layout, integrated Components, suitable and responsive for all screens. Using Next.js, TypeScript, TailwindCSS, Syncfusion, Chart.js, Swiper.js, React icons.',
+    liveLink: 'https://dashboard-ui-kit.vercel.app',
+    githubLink: 'https://github.com/abdelfattah90',
+    image: './images/projects/dashboard-ui-kit.png',
+  },
+  {
+    title: 'eCommerce MERN Stack App',
+    description:
+      'Shopping cart, product search, upload image, admin area and PayPal integration. Using React vite, Redux Toolkit, Axios, Node.js, Express, JWT, MongoDB.',
+    liveLink: 'https://proshop-tpqo.onrender.com',
+    githubLink: 'https://github.com/abdelfattah90',
+    image: './images/projects/proshop.png',
+  },
+  {
+    title: 'Apple Store',
+    description:
+      'Multi Languages app, manage and share state by Context API. Using Next.js, TypeScript, TailwindCSS, React-i18next.',
+    liveLink: 'https://appple.vercel.app',
+    githubLink: 'https://github.com/abdelfattah90',
+    image: './images/projects/apple.png',
+  },
+  {
+    title: 'السيرة النبوية',
+    description:
+      'Using flutter cross platform, shared preferences, and considerate interface segregation principle.',
+    liveLink:
+      'https://play.google.com/store/apps/details?id=com.seira.seiranabawayaa',
+
+    image: './images/projects/seira.png',
+  },
+]
+
 function App() {
   return (
     <>
@@ -19,163 +55,52 @@ function App() {
           </div>
         </div>
       </section>
+
       <section className='container'>
         <section className='pt-5 pb-5'>
-          <div className='p-5 m-3 d-none d-sm-block d-sm-none d-md-block'></div>
           <h1 className='pb-5 text-center'>Projects</h1>
-          <div className='row justify-content-center'>
-            {/* Start Card col- */}
-            <div className='p-5 col-sm-6'>
-              <div className='card card-transition'>
-                <img
-                  src='./images/projects/dashboard-ui-kit.png'
-                  className='card-img-top'
-                  alt='app'
-                />
-                <div className='card-body d-flex flex-column'>
-                  <h4 className='card-title'>Next.js Dashboard UI Kit</h4>
-                  <p className='card-text text-muted mb-4 p-3'>
-                    Designed initial layout, integrated Components, suitable and
-                    responsive for all screens. Using Next.js, TypeScript,
-                    TailwindCSS, Syncfusion, Chart.js, Swiper.js, React icons.
-                  </p>
-                  <div className='btn-container'>
-                    <a
-                      href='https://dashboard-ui-kit.vercel.app'
-                      className='btn button-live'
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      Live
-                    </a>
-                    <a
-                      href='https://github.com/abdelfattah90'
-                      className=''
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      <img
-                        src='./images/github-dark.png'
-                        width={50}
-                        height={50}
-                      />
-                    </a>
+          <div className='row'>
+            {projects.map((project, index) => (
+              <div className='p-3 col-md-6' key={index}>
+                <div className='card card-transition m-4 '>
+                  <img
+                    src={project.image}
+                    className='card-img-top'
+                    alt={project.title}
+                  />
+                  <div className='card-body d-flex flex-column'>
+                    <h4 className='card-title lang-ar'>{project.title}</h4>
+                    <p className='card-text text-muted  mb-4 p-3'>
+                      {project.description}
+                    </p>
+                    <div className='btn-container'>
+                      <a
+                        href={project.liveLink}
+                        className='btn button-live'
+                        target='_blank'
+                        rel='noreferrer'
+                      >
+                        Live
+                      </a>
+                      {project.githubLink && (
+                        <a
+                          href={project.githubLink}
+                          target='_blank'
+                          rel='noreferrer'
+                        >
+                          <img
+                            src='./images/github-dark.png'
+                            width={50}
+                            height={50}
+                            alt='GitHub'
+                          />
+                        </a>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            {/* End Card col- */}
-
-            {/* Start Card col- */}
-            <div className='p-5 col-sm-6'>
-              <div className='card card-transition'>
-                <img
-                  src='./images/projects/proshop.png'
-                  className='card-img-top'
-                  alt='app'
-                />
-                <div className='card-body d-flex flex-column'>
-                  <h4 className='card-title'>eCommerce MERN Stack App</h4>
-                  <p className='card-text text-muted mb-4 p-3'>
-                    Shopping cart, product search, upload image, admin area and
-                    PayPal integration. Using React vite, Redux Toolkit, Axios,
-                    Node.js, Express, JWT, MongoDB.
-                  </p>
-                  <div className='btn-container'>
-                    <a
-                      href='https://proshop-tpqo.onrender.com'
-                      className='btn button-live'
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      Live
-                    </a>
-                    <a
-                      href='https://github.com/abdelfattah90'
-                      className=''
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      <img
-                        src='./images/github-dark.png'
-                        width={50}
-                        height={50}
-                      />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* End Card col- */}
-
-            {/* Start Card col- */}
-            <div className='p-5 col-sm-6'>
-              <div className='card card-transition'>
-                <img
-                  src='./images/projects/apple.png'
-                  className='card-img-top'
-                  alt='app'
-                />
-                <div className='card-body d-flex flex-column'>
-                  <h4 className='card-title'>Apple Store</h4>
-                  <p className='card-text text-muted mb-4 p-3'>
-                    Multi Languages app, manage and share state by Context API.
-                    Using Next.js, TypeScript, TailwindCSS, React-i18next.
-                  </p>
-                  <div className='btn-container'>
-                    <a
-                      href='https://appple.vercel.app'
-                      className='btn button-live'
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      Live
-                    </a>
-                    <a
-                      href='https://github.com/abdelfattah90'
-                      className=''
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      <img
-                        src='./images/github-dark.png'
-                        width={50}
-                        height={50}
-                      />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* End Card col- */}
-
-            {/* Start Card col- */}
-            <div className='p-5 col-sm-6'>
-              <div className='card card-transition'>
-                <img
-                  src='./images/projects/seira.png'
-                  className='card-img-top'
-                  alt='app'
-                />
-                <div className='card-body d-flex flex-column'>
-                  <h4 className='card-title lang-ar'>السيرة النبوية</h4>
-                  <p className='card-text text-muted mb-4 p-3'>
-                    Using flutter cross platform, shared preferences, and
-                    considerate interface segregation principle.
-                  </p>
-
-                  <a
-                    href='https://play.google.com/store/apps/details?id=com.seira.seiranabawayaa'
-                    className='btn button-live'
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    Live
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* End Card col- */}
+            ))}
           </div>
         </section>
       </section>
